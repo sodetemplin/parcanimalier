d85519c953d383df3224fe69daa3ec5d61fb7d03
david.rousse@gmail.com <david.rousse@gmail.com>
2013-02-20 12:17:36 +0100

    Mise à jour rep. racine

--- faune-pyreneenne/include/en/footer.inc.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-	<a href="../"><span class="icon gray small" data-icon="I"></span></a> - &copy; Copyright <?php echo date('Y'); ?> - <?php $titre_page = (isset($_GET['langue']) ? $titre_site_par_langues[$_GET['langue']] : $titre_site_par_langues[$langue_par_defaut] ); echo $titre_page; ?> - Phone : <?php echo $telephone_en; ?> - Email : <?php echo $mail_contact; ?><a id="link-top" href="#top-of-page">Back to top</a>
-</div>
--- faune-pyreneenne/include/footer.inc.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-	<a href="../"><span class="icon gray small" data-icon="I"></span></a> - &copy; Tous droits réservés <?php echo date('Y'); ?> - <?php $titre_page = (isset($_GET['langue']) ? $titre_site_par_langues[$_GET['langue']] : $titre_site_par_langues[$langue_par_defaut] ); echo $titre_page; ?> - Tél : <?php echo $telephone_fr; ?> - Email : <?php echo $mail_contact; ?><a id="link-top" href="#top-of-page">Retour en haut</a>
-</div>
