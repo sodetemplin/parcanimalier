bb0d37ac30d18504fadbb8aac3507741aa8b3a74
david.rousse@gmail.com <david.rousse@gmail.com>
2013-02-26 17:27:26 +0100

    Correction appel email

--- parc-animalier/contenu/pages/contact_parc.php
+++ parc-animalier/contenu/pages/contact_parc.php
@@ -4,7 +4,7 @@
 <li><a href="./index.php?page=accueil_parc">Accueil</a></li>
 <li>Nous écrire</li>
 </ul>
-	<p>Nous sommes joignables par téléphone au <strong><?php echo traduction($langue,'telephone'); ?></strong>. Vous pouvez également nous écrire à <strong><?php echo email(); ?></strong></p>
+	<p>Nous sommes joignables par téléphone au <strong><?php echo traduction($langue,'telephone'); ?></strong>. Vous pouvez également nous écrire à <strong><?php echo email(''); ?></strong></p>
 	<p>Nous sommes situés à l'adresse suivante (plan disponible <a href="./index.php?page=plan_acces&langue=fr">ici</a>) :
 	<address><?php echo $adresse_postale; ?></<address>		
 </div>
--- parc-animalier/contenu/pages/en/park_contact.php
+++ parc-animalier/contenu/pages/en/park_contact.php
@@ -4,7 +4,7 @@
 <li><a href="./index.php?page=park_home&langue=en">Home</a></li>
 <li>Contact us</li>
 </ul>
-	<p>Our phone number is <strong><?php echo traduction($langue,'telephone'); ?></strong>. You can also send us an email to <strong><?php echo email(); ?></strong></p>
+	<p>Our phone number is <strong><?php echo traduction($langue,'telephone'); ?></strong>. You can also send us an email to <strong><?php echo email(''); ?></strong></p>
 	<p>Our address is (map available <a href="./index.php?page=access_map&langue=en">here</a>) :<address><?php echo $adresse_postale; ?></address></p>
 
 
