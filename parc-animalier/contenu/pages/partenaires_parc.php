e73ef8e19d580bd4027507e0bad59b6ee44e6bcc
david.rousse@gmail.com <david.rousse@gmail.com>
2013-03-12 16:25:44 +0100

    Validaiton

--- parc-animalier/contenu/pages/partenaires_parc.php
+++ parc-animalier/contenu/pages/partenaires_parc.php
@@ -13,7 +13,8 @@
 		  <li><a href="http://www.tripadvisor.fr/Attraction_Review-g635572-d2256090-Reviews-Parc_animalier_des_Angles-Les_Angles_Pyrenees_Orientales_Languedoc_Roussillon.html" target="_blank">Le parc animalier sur TripAdvisor</a></li>
         <li><a href="http://www.formigueres.fr/parc-animalier/les-angles/tabid/37095/offreid/949ba41b-79aa-43a4-8f3d-0ee323ad6eb3/detail.aspx" target="_blank">Le parc animalier sur le site de Formiguères</a></li>
         <li><a href="http://www.facebook.com/ParcAnimalierDesAngles" target="_blank">Le parc animalier sur Facebook</a></li>
-		  <li><a href="http://www.dailymotion.com/video/xaeiln_parc-animalier-des-angles-pyrenees_animals#.USx0AdFJ4ew" target="_blank">Le parc animalier en vidéo sur Dailymotion</a></li>          					
+		  <li><a href="http://www.dailymotion.com/video/xaeiln_parc-animalier-des-angles-pyrenees_animals#.USx0AdFJ4ew" target="_blank">Le parc animalier en vidéo sur Dailymotion</a></li>
+		  <li><a href="http://www.futura-sciences.com/fr/environnement/exploregions/escapade/r/pyrenees-orientales-1/d/parc-animalier_1837/" target="_blank">Le parc animalier sur futura-sciences.com</a></li>          							            					
 	</ul>	
 	</div>
 	<div class="col_6">
@@ -25,6 +26,7 @@
         <li><a href="http://www.font-romeu.fr/fr/syndication/a-decouvrir-autour-de-font-romeu-ete/detail/PCULAR066FS0006F/LES-ANGLES/PARC-ANIMALIER-DES-ANGLES" target="_blank">Le parc animalier sur le site de Font Romeu</a></li>
         <li><a href="https://plus.google.com/100015414012897778412/about?gl=fr&hl=fr" target="_blank">Le parc animalier sur Google+</a></li>
         <li><a href="http://www.pagesjaunes.fr/sorties/parc-animalier-des-angles-17676" target="_blank">Le parc animalier sur les Pages Jaunes</a></li>
+		  <li><a href="http://www.capcir-pyrenees.com/Parc-animalier-des-Angles--5540.phtm" target="_blank">Le parc animalier sur capcir-pyrenees.com</a></li>          							            					        
         <li><span class="icon grey small" data-icon="@"></span> <a href="./index.php?page=contact_parc"> Contacter nous pour être ajouté à cette page !</a></li>          
 	</ul>	
 	</div>
